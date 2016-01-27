b71f7b5cc25c0b7b3571cd5e3201f3da06d75b45
Kevin Pasquarella <pasquarella4292@gmail.com>
2016-01-26 21:00:46 -0500

    user functionality update

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,3 @@
 class School < ActiveRecord::Base
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+
+	def user
+		gpa = gets
+		sat = gets
+		act = gets
+		location = gets
+		population = gets
+		social_life = gets
+		athletics = gets
+	end
+
+	def user_to_db
+		user = User.create(location: user(location),
+			population: user(population), gpa: user(gpa),
+			sat: user(sat), act: user(act),
+			social_life: user(social_life),
+			athletics: user(athletics))
 end
