ec0c49188361b56bbf4741e859b90b4f66d688af
Kevin Pasquarella <pasquarella4292@gmail.com>
2016-01-30 15:49:27 -0500

    added user and school functionality

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,35 @@
 class School < ActiveRecord::Base
+	# Create new school
+	# Will add admin privilege so schools cannot
+	# be created, deleted, or edited without
+	# admin credentials.
+	def newSchool
+		name = gets
+		gpa = gets
+		sat = gets
+		act = gets
+		location = gets
+		population = gets
+		social_life = gets
+		athletics = gets
+	end
 
+	# Save school info to the db
+	def saveSchool(newSchool)
+		school = School.create(name: newSchool.name,
+			gpa: newSchool.gpa,
+			sat: newSchool.sat,
+			act: newSchool.act,
+			location: newSchool.location,
+			population: newSchool.population,
+			social_life: newSchool.social_life,
+			athletics: newSchool.athletics)
+	end
+
+	# Delete school from db
+	def deleteSchool(schoolName)
+		schoolName = gets
+		school = School.find_by(name: schoolName)
+		school.destroy
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
-	def user
+	# Gets the info from the user
+	def newUser
+		name = gets
 		gpa = gets
 		sat = gets
 		act = gets
@@ -9,11 +11,22 @@ class User < ActiveRecord::Base
 		social_life = gets
 		athletics = gets
 	end
+	# Takes the info given by the user and saves
+	# it into the database
+	def user_to_db(newUser)
+		user = User.create(name: newUser.name,
+			location: newUser.location,
+			population: newUser.population,
+			gpa: newUser.gpa,
+			sat: newUser.sat, act: newUser.act,
+			social_life: newUser.social_life,
+			athletics: newUser.athletics)
+		user.save
+	# Deletes the user from the database
+	def user_delete(username)
+		username = gets
+		user = User.find_by(name: username)
+		user.destroy
+	end
 
-	def user_to_db
-		user = User.create(location: user(location),
-			population: user(population), gpa: user(gpa),
-			sat: user(sat), act: user(act),
-			social_life: user(social_life),
-			athletics: user(athletics))
 end
