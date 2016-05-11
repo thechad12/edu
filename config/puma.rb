9a5f37c346077fe6bf7f2e4a1cdab33a674a391d
Kevin Pasquarella <pasquarella4292@gmail.com>
2016-05-10 20:07:54 -0400

    puma.rb changed

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,16 @@
 require "active_record"
-cwd = File.dirname(__FILE__)+"/.."
-ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RACK_ENV"]])
-ActiveRecord::Base.verify_active_connections!
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
